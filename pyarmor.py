5de932ccc2a052b2ceb8702330957333b9393106
Caio <caio14.adm@proton.me>
2025-03-30 12:45:58 -0300

    Add Files

---
+++ pyarmor.py
@@ -0,0 +1,15 @@
+from pyfiglet import figlet_format
+from pystyle import System, Colorate, Colors
+import subprocess
+import msvcrt
+logo = figlet_format("PyArmor Obfuscator")
+print(Colorate.Vertical(Colors.rainbow, logo))
+for i in range(5):
+    print()
+file_path = input(Colorate.Horizontal(Colors.cyan_to_green, "[+] Drag ur Python file -> "))
+System.Clear()
+pyarmor_logs = subprocess.run(["pyarmor", "gen", file_path.replace('"', '')], capture_output=True, text=True)
+for line in pyarmor_logs.stderr.splitlines():
+    print(Colorate.Horizontal(Colors.rainbow, line))
+print(Colorate.Horizontal(Colors.red_to_blue, "[+] Done Obfuscating"))
+msvcrt.getch()
